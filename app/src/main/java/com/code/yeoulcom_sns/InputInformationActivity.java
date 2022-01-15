ad4eec1397fd0d1161aea6fccd3f11c58d594246
DongMin-0604 <dongmin3147@gmail.com>
2022-01-15 13:20:34 +0900

    필요한 activity 생성, activity에 연동될 java 파일 생성, titlebar 제거, 상태바 색상 변경

---
+++ app/src/main/java/com/code/yeoulcom_sns/InputInformationActivity.java
@@ -0,0 +1,16 @@
+package com.code.yeoulcom_sns;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.yeoulcom_sns.R;
+
+public class InputInformationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_information);
+    }
+}
---
+++ app/src/main/java/com/code/yeoulcom_sns/IntroActivity.java
@@ -0,0 +1,16 @@
+package com.code.yeoulcom_sns;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.yeoulcom_sns.R;
+
+public class IntroActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
