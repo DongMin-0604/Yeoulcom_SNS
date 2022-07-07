974623e0f7188a8f433dd1bc4402dd5cfd004c93
notebook <dongmin3147@gmail.com>
2022-07-07 09:14:07 +0900

    회장 권한 이름 뒤에 "회장" 추가, 이슈 없음

--- app/src/main/java/com/code/yeoulcom_sns/InputInformationActivity.java
+++ app/src/main/java/com/code/yeoulcom_sns/InputInformationActivity.java
@@ -132,7 +132,7 @@ public class InputInformationActivity extends AppCompatActivity {
                     et_password.setEnabled(true);
 
                     //이름,기수 변수에 값 넣어주기
-                    st_name = et_name.getText().toString();
+                    st_name = et_name.getText().toString() +" "+"회장";
                     st_generation = sp_generation.getSelectedItem().toString();
 
                     //화면 전환, 기수,이름,권한 정보 다음 엑티비티로 넘기기
